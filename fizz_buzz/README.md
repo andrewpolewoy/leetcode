f9e8b715f9967819a1ddb726c953b7f2524bffe1
andrewpolewoy <andrewpolewoy@gmail.com>
2023-01-12 13:12:02 +0300

    412. Fizz Buzz

---
+++ fizz_buzz/README.md
@@ -0,0 +1,23 @@
+#412. Fizz Buzz
+<br><b>Easy</b>
+
+Given an integer n, return a string array answer (1-indexed) where:<br>
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.<br>
+answer[i] == "Fizz" if i is divisible by 3.<br>
+answer[i] == "Buzz" if i is divisible by 5.<br>
+answer[i] == i (as a string) if none of the above conditions are true.<br><br>
+<b>Example 1:<br>
+Input: n = 3<br>
+Output: ["1","2","Fizz"]<br>
+
+Example 2:<br>
+Input: n = 5<br>
+Output: ["1","2","Fizz","4","Buzz"]<br>
+
+Example 3:<br>
+Input: n = 15<br>
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]<br>
+
+Constraints:<br>
+1 <= n <= 104
---
+++ fizz_buzz/Solution.java
@@ -0,0 +1,24 @@
+package fizz_buzz;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[][] arr = new int[][]{
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 60}
+        };
+        System.out.println((maximumWealth(arr)));
+    }
+
+    public static int maximumWealth(int[][] accounts) {
+        int wealthSoFar = 0;
+        for (int i = 0; i < accounts.length; i++) {
+            int wealth = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                wealth += accounts[i][j];
+            }
+            if (wealth > wealthSoFar) wealthSoFar = wealth;
+        }
+        return wealthSoFar;
+    }
+}
